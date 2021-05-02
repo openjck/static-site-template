f19297a97fa1a0599fbde86f27dd6a285024129e
John Karahalis <john.karahalis@gmail.com>
2021-05-02 17:03:02 -0400

    Disable no-descending-specificity stylelint rule

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: "stylelint-config-standard",
   plugins: ["stylelint-order"],
   rules: {
+    "no-descending-specificity": null,
     "order/properties-alphabetical-order": true,
   },
 };
