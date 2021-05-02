7e87693169f7a2dca8bd639736af122d79e4f63a
John Karahalis <john.karahalis@gmail.com>
2021-05-02 18:32:42 -0400

    Remove stylelint-order plugin
    It turns out this is too annoying. I like keeping properties in order,
    but there are cases where this doesn't make much sense.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,8 +1,6 @@
 module.exports = {
   extends: "stylelint-config-standard",
-  plugins: ["stylelint-order"],
   rules: {
     "no-descending-specificity": null,
-    "order/properties-alphabetical-order": true,
   },
 };
