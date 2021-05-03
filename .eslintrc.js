db70ab527670b9950117481ea9d51c5215604e1c
John Karahalis <john.karahalis@gmail.com>
2021-05-02 23:24:22 -0400

    Add ESLint, Prettier/Stylelint integration

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base", "plugin:json/recommended", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "json"],
+};
