ecfc04e541a96e4f4ff10347e7f26c07e0ddacf6
John Karahalis <john.karahalis@gmail.com>
2021-09-26 01:46:43 -0400

    Remove ecmaVersion in .eslintrc.js
    According to the ESLint documentation, this is automatically set when
    "es2021" is an environment.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,6 @@ module.exports = {
   extends: ["airbnb-base", "plugin:json/recommended", "prettier"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 12,
     sourceType: "module",
   },
   plugins: ["@typescript-eslint", "json"],
