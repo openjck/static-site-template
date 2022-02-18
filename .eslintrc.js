5685da57a35ce2378f96ff9817b0856459129545
John Karahalis <john.karahalis@gmail.com>
2022-02-17 22:40:53 -0500

    Various ESLint configuration improvements
    The move from .eslintignore to the `ignorePatterns` property of
    .eslintrc.js will make it easier to distribute this configuration as a
    package in the future, if I ever choose to do so.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,59 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["airbnb-base", "plugin:json/recommended", "prettier"],
+  extends: ["airbnb-base", "prettier"],
+  ignorePatterns: [
+    // Generated
+    "/dist/",
+
+    // Inverse matches (DO lint these)
+    "!/.eslintrc.js",
+    "!/.posthtmlrc.js",
+    "!/.prettierrc.js",
+    "!/.stylelintrc.js",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     sourceType: "module",
   },
   plugins: ["@typescript-eslint", "json"],
+  rules: {
+    // Keep imports organized
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "unknown",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+        "newlines-between": "always",
+        warnOnUnassignedImports: true,
+      },
+    ],
+  },
   overrides: [
-    // Allow devDependencies to be imported in the .posthtmlrc.js configuration
-    // file
+    // Allow devDependencies to be imported in certain configuration files
     {
-      files: [".posthtmlrc.js"],
+      files: [".posthtmlrc.js", "vite.{js,ts}"],
       rules: {
         "import/no-extraneous-dependencies": [
           "error",
           { devDependencies: true },
         ],
       },
+    },
+
+    // JSON
+    {
+      files: ["*.json"],
+      extends: ["plugin:json/recommended"],
     },
   ],
 };
